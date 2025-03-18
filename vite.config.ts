689e6a9b9558cacd571067102eaa5e70f9788af5
Wechuli Simiyu <wechuli017@gmail.com>
2025-03-18 15:00:04 +0300

    added mdx support

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,21 @@ import adonisjs from '@adonisjs/vite/client'
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 import Components from 'unplugin-vue-components/vite'
+import remarkGfm from 'remark-gfm'
+import rehypeHighlight from 'rehype-highlight'
+import mdx from '@mdx-js/rollup'
 
 export default defineConfig({
   plugins: [
     inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.ts' } }),
+    {
+      enforce: 'pre',
+      ...mdx({
+        providerImportSource: '@mdx-js/vue',
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [rehypeHighlight],
+      }),
+    },
     vue(),
     adonisjs({ entrypoints: ['inertia/app/app.ts'], reload: ['resources/views/**/*.edge'] }),
     Components({
