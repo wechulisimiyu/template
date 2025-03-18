4af7ed0d9c2651de057967c4feaaa8c3927a512d
Wechuli Simiyu <wechuli017@gmail.com>
2025-03-18 18:28:02 +0300

    fix email verification migration

--- database/migrations/1742291380381_create_email_verifications_table.ts
+++ database/migrations/1742291380381_create_email_verifications_table.ts
@@ -6,9 +6,10 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
+      table.integer('user_id').unsigned().references('users.id').onDelete('CASCADE')
+      table.string('token').unique()
+      table.timestamp('expires_at')
+      table.timestamps(true, true)
     })
   }
 
